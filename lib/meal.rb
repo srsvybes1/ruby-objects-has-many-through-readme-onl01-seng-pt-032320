c5b4aa1c13142644727d12ddb83a302dca1015e2
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-04 15:30:20 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,14 @@
 class Meal
-
+    attr_accessor :waiter, :tip, :total, :customer
+    @@all = []
+    def initialize(waiter, tip=0, total, customer)
+      @waiter = waiter
+      @tip = tip
+      @customer = customer
+      @total = total
+      @@all << self
+    end
+    def self.all
+      @@all
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,23 @@
 class Waiter
-
+  attr_accessor :name, :age
+  @@all = []
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  def self.all
+    @@all
+end
+def meals
+  Meal.all.select do |meal|
+    meal.waiter == self
+  end
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    best_tipped_meal.customer
+end
+end
 end
