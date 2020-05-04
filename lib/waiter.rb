fd76fb7e8e143e31829c204d0c75d06569419220
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-04 16:54:57 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,16 +1,16 @@
 class Waiter
-  attr_accessor :name, :age
+  attr_accessor :name, :yrs_experience 
   @@all = []
-  def initialize(name, age)
+  def initialize(name, yrs_experience)
     @name = name
-    @age = age
+    @yrs_experience = yrs_experience
     @@all << self
   end
   def self.all
     @@all
 end
 def new_meal(customer, total, tip=0)
-  Meal.new(self, total, tip)
+  Meal.new(self, customer, total, tip)
 end
 def meals
   Meal.all.select do |meal|
