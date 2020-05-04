3e65d1b76578ae102879f3c393be2b2c9c1459da
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-04 16:04:54 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -9,9 +9,9 @@ class Waiter
   def self.all
     @@all
 end
-def new_meal(waiter, total, tip=0)
-  Waiter.new(waiter, self, total, tip)
-end
+#def new_meal(waiter, total, tip=0)
+#  Waiter.new(waiter, self, total, tip)
+#end
 
 end
 def meals
